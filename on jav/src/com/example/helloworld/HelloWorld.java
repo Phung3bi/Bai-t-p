8900171984dabfccca060e613e16265381cea9ef
phung <lekimphungbb@gmail.com>
2022-11-03 10:36:20 +0700

    Initialize project

---
+++ on jav/src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,22 @@
+package com.example.helloworld;
+public class HelloWorld {
+//   public static void main(String[] args) {
+//        System.out.println("hello phung3bi");
+//    }
+//}
+
+    public static void main(String[] args) {
+        int tong = 0;
+        int i = 1;
+        while (i <= 100) {
+            tong = tong + i;
+            i++;
+        }
+        System.out.println(tong);
+    }
+}
+
+//    public static void main(String[] args ){
+//    int songuyen;
+//    }
+//}
